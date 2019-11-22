8ffbca85ee2f6ed97d885c97f4de8fa15b5609db
foka <michal.foka.wlodarczyk@gmail.com>
2019-11-22 14:41:09 +0100

    Initiate empty app end empty test project

---
+++ FizzBuzzApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FuzzBazzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
